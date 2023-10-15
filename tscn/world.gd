231afcc69c403f753e059f3897a47b892a237411
hione0413 <hione0413@gmail.com>
2023-10-15 22:46:31 +0900

    game start countdown

--- tscn/world.gd
+++ tscn/world.gd
@@ -8,6 +8,10 @@ extends Node2D
 #@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
+
 
 
 # 1: 기본적인 필드 및 플레이어 생성
@@ -22,28 +26,40 @@ extends Node2D
 #10: Start menu
 #11: UI Theme
 #12: Wall jump fix
+#13: game start countdown
 
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK)
+#	RenderingServer.set_default_clear_color(Color.BLACK)
 #	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
+	
+	start_in.visible = true
+	
+	get_tree().paused = true # 타이머 실행 전에는 일단 멈춰 둠
+	await LevelTransition.fade_from_black()
+	animation_player.play("countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
+	
+	start_in.visible = false
 	
 
 func show_level_completed():
 	level_completed.show()
-	
-#   다음 레벨 없으면 return
-	if not next_level is PackedScene: return
+
 	get_tree().paused = true
 	
 	# 레벨 클리어 후 잠시 멈춤
 	await get_tree().create_timer(0.5).timeout
+	#   다음 레벨 없으면 return
+	if not next_level is PackedScene: return
 	
 	await LevelTransition.fade_to_black()
 #   다음레벨로 넘어감
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
-	await LevelTransition.fade_from_black()
+	
 	
 #	get_tree().paused = true
-	
+
+
